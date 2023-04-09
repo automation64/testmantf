0deac20cbc30bfb32c75ad2809cc961ae57491c7
serdigital64 <serdigital64@gmail.com>
2023-04-09 10:02:38 -0300

    dev: update cicd scripts

--- test/lib/batscore-setup.bash
+++ test/lib/batscore-setup.bash
@@ -4,9 +4,9 @@
 # * Source this file from the first line of the setup() function in the test-case
 #
 
-. "$TESTMANTF_CMD_BATS_HELPER_SUPPORT"
-. "$TESTMANTF_CMD_BATS_HELPER_ASSERT"
-. "$TESTMANTF_CMD_BATS_HELPER_FILE"
+. "$TESTMANSH_CMD_BATS_HELPER_SUPPORT"
+. "$TESTMANSH_CMD_BATS_HELPER_ASSERT"
+. "$TESTMANSH_CMD_BATS_HELPER_FILE"
 
 # Do not overwrite signals already set by bats-core
 # ERR, DEBUG, EXIT
@@ -17,4 +17,4 @@ set +o 'nounset'
 # Do not set/unset: 'keyword', 'noexec'
 
 # (Optional) Add shared settings. Available to all test-cases using this setup routine
-. "${TESTMANTF_PROJECT_ROOT}/bin/devtmtf-set"
+. "${TESTMANSH_PROJECT_BIN}/dev-set"
